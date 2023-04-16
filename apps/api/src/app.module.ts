72b06822835919a86b0837573df874ba53e6add9
vadym <checherinda.vadym@webbylab.com.com>
2023-04-17 01:14:26 +0300

    Add process microservice

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Client, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
