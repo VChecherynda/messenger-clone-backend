51b7919effe78858a48de935999569f05bb42941
vadym <checherinda.vadym@webbylab.com.com>
2023-04-19 00:24:34 +0300

    Fix shared module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from '@app/shared';
-import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -11,59 +10,13 @@ import { ClientProxyFactory, Transport } from '@nestjs/microservices';
       isGlobal: true,
       envFilePath: './.env',
     }),
-    // SharedModule.registerRmq('AUTH_SERVICE', process.env.RABBITMQ_AUTH_QUEUE),
-    // SharedModule.registerRmq(
-    //   'PRESENCE_QUEUE',
-    //   process.env.RABBITMQ_PRESENCE_QUEUE,
-    // ),
+    SharedModule.registerRmq('AUTH_SERVICE', process.env.RABBITMQ_AUTH_QUEUE),
+    SharedModule.registerRmq(
+      'PRESENCE_QUEUE',
+      process.env.RABBITMQ_PRESENCE_QUEUE,
+    ),
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: 'AUTH_SERVICE',
-      inject: [ConfigService],
-      useFactory(configService) {
-        const USER = configService.get('RABBITMQ_USER');
-        const PASS = configService.get('RABBITMQ_PASS');
-        const HOST = configService.get('RABBITMQ_HOST');
-        const QUEUE = configService.get('RABBITMQ_AUTH_QUEUE');
-
-        return ClientProxyFactory.create({
-          transport: Transport.RMQ,
-          options: {
-            urls: [`amqp://${USER}:${PASS}@${HOST}`],
-            noAck: false,
-            queue: QUEUE,
-            queueOptions: {
-              durable: true,
-            },
-          },
-        });
-      },
-    },
-    {
-      provide: 'PRESENCE_SERVICE',
-      inject: [ConfigService],
-      useFactory(configService) {
-        const USER = configService.get('RABBITMQ_USER');
-        const PASS = configService.get('RABBITMQ_PASS');
-        const HOST = configService.get('RABBITMQ_HOST');
-        const QUEUE = configService.get('RABBITMQ_PRESENCE_QUEUE');
-
-        return ClientProxyFactory.create({
-          transport: Transport.RMQ,
-          options: {
-            urls: [`amqp://${USER}:${PASS}@${HOST}`],
-            noAck: false,
-            queue: QUEUE,
-            queueOptions: {
-              durable: true,
-            },
-          },
-        });
-      },
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
